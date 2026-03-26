c66c1f7943dbec079df25e88c1ceedf46fb8478f
Eduardo Cadenas Sarabia <cadenas_edu@hotmail.com>
2026-03-25 18:01:31 -0600

    Proyecto final de lista de tareas con React y TypeScript

---
+++ src/components/ListaTareas.tsx
@@ -0,0 +1,17 @@
+import { Tarea } from "./Tarea"
+
+type ListaTareas = {
+    listaTareas: string[]
+    borrarTarea: (index: number) => void
+}
+
+export const ListaTareas = ({listaTareas, borrarTarea}: ListaTareas) => {
+  return (
+    <div className="taskList">
+        {listaTareas.map((task, index) => (
+            <Tarea key={index} tarea={task} borrarTarea={() => borrarTarea(index)} />
+        )
+        )}   
+    </div>
+  )
+}   
